1acf4ada95592594fd4a7302f4c10d0a83d366b2
Alok panday <32874450+Alok420@users.noreply.github.com>
2022-03-05 14:50:20 +0530

    Create lecture3.php

---
+++ lecture3.php
@@ -0,0 +1,24 @@
+<?php
+
+$my_budget="80000";
+$fee="50000";
+echo "< operator".$fee<20000;
+echo "> operator".$fee>20000;
+echo "== operator".$fee==20000;
+echo "!= operator".$fee!=20000;
+echo "<= operator".$fee<=20000;
+echo ">= operator".$fee>=20000;
+
+if($fee==$my_budget){
+    echo "<br>I will join this institute";
+}else{
+    echo "<br>I will  not join this institute";
+}
+
+
+
+
+
+
+
+?>
