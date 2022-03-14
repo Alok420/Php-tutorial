60c3f58eeddf5e74d5d896f7cf27b2fca2f6b568
Alok panday <32874450+Alok420@users.noreply.github.com>
2022-03-14 15:27:13 +0530

    Create lecture6.php

---
+++ lecture6.php
@@ -0,0 +1,33 @@
+<?php 
+$weakday="sunday";
+switch($weakday){
+    case "sunday":{
+        echo "It's a joyful day";
+        break;
+    }
+    case "monday":{
+        echo "It's a working first day";
+        break;
+    }
+    case "tuesday":{
+        echo "It's a hanuman's day";
+        break;
+    }
+    case "wednesday":{
+        echo "It's a hectic day";
+        break;
+    }
+    case "thursday":{
+        echo "It's a god's  day";
+        break;
+    }
+    case "friday":{
+        echo "It's a doddes sharawaty day";
+        break;
+    }
+    case "saturday":{
+        echo "It's a party day";
+        break;
+    }
+}
+?>
